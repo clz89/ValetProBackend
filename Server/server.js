9de0ad342ae8116c5dd0a8355177648042edacc7
clz89 <96872883+clz89@users.noreply.github.com>
2022-12-18 13:26:16 -0800

    hjk

--- Server/server.js
+++ Server/server.js
@@ -44,7 +44,7 @@ require("./routes/auth.routes")(app);
 require("./routes/user.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
